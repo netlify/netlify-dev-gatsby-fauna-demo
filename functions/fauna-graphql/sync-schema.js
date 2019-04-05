af4fa7c060cd40c673ed1fc373f216b91cb888d1
sw-yx <swyx@dontemail.me>
2019-04-05 01:55:13 -0400

    gatsbyfaunademo

--- functions/fauna-graphql/sync-schema.js
+++ functions/fauna-graphql/sync-schema.js
@@ -3,16 +3,16 @@
 /* sync GraphQL schema to your FaunaDB account - use with `netlify dev:exec <path-to-this-file>` */
 function createFaunaGraphQL() {
   if (!process.env.FAUNADB_SERVER_SECRET) {
-    console.log("No FAUNADB_SERVER_SECRET in environment, skipping DB setup")
+    console.log("No FAUNADB_SERVER_SECRET in environment, skipping DB setup");
   }
-  console.log("Upload GraphQL Schema!")
+  console.log("Upload GraphQL Schema!");
 
-  var request = require("request")
-  const fs = require("fs")
-  const path = require("path")
+  var request = require("request");
+  const fs = require("fs");
+  const path = require("path");
   var dataString = fs
     .readFileSync(path.join(__dirname, "schema.graphql"))
-    .toString() // name of your schema file
+    .toString(); // name of your schema file
 
   var options = {
     url: "https://graphql.faunadb.net/import",
@@ -20,21 +20,20 @@ function createFaunaGraphQL() {
     body: dataString,
     auth: {
       user: process.env.FAUNADB_SERVER_SECRET,
-      pass: "",
-    },
-  }
+      pass: ""
+    }
+  };
 
-  request(options, callback)
+  request(options, callback);
 
   function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-      // console.log("body", body)
-    }
-    // for debugging
-    else {
-      console.log({ error, body })
+      // // for debugging
+      // console.log("body", body);
+    } else {
+      console.error("something wrong happened: ", { error, body });
     }
   }
 }
 
-createFaunaGraphQL()
+createFaunaGraphQL();
